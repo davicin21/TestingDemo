5dfedf61fee47cdfb751baa5ce6c0c8beb756b52
David Alvarez <davidao01@educastur.es>
2024-10-07 09:28:53 +0200

    Pruebas

--- MyLibrary/Operations.cs
+++ MyLibrary/Operations.cs
@@ -1,4 +1,6 @@
-﻿namespace MyLibrary
+﻿using System.Collections;
+
+namespace MyLibrary
 {
     public class Operations
     {
@@ -16,5 +18,30 @@
         {
             return number1 + number2;
         }
+
+
+        //ArrayList= List
+        public IList<int> GetEvenNumbers(int start, int end)
+        {
+            //List<int> numbers = new List<int>();
+            //for (int i = start; i < end; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        numbers.Add(i);
+            //    }
+            //}
+            //return numbers;
+
+            //List<int> numbers = Enumerable.Range(start, end)
+            //    .Where(number => number % 2 == 0)
+            //    .ToList();
+
+            return Enumerable.Range(start, end)
+                .Where(Iseven)
+                .ToList();
+        }
+
+        //Metodo de prueba para la clase sobre el array para ver que numeros devuelve, hay que usar metodos de prueba de unit para arrays
     }
 }
---
+++ MyLibraryTest/CustomerNunitTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    [TestFixture]
+    class CustomerNunitTest
+    {
+        [Test]
+        public void FullName_ReturnEmpty()
+        {
+            //1. Arrange
+            Customer customer = new Customer();
+
+            //2. Act
+            string result = customer.Fullname;
+
+            //3. Assert
+            Assert.That(String.IsNullOrWhiteSpace(result), Is.True);
+        }
+
+        [Test]
+        public void FullName_ReturnFullName()
+        {
+            //1. Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            //2. Act
+            string result = customer.Fullname;
+
+            //3. Assert
+            Assert.That(result, Is.Not.WhiteSpace);
+            Assert.That(result, Does.Contain(" "));
+            Assert.That(result, Does.Contain("John"));
+            Assert.That(result, Does.Contain("Doe"));
+            Assert.That(result, Does.StartWith("John"));
+            Assert.That(result, Does.EndWith("Doe"));
+            Assert.That(result, Is.EqualTo("John Doe"));
+            Assert.That(result, Is.EqualTo("john doe").IgnoreCase);
+        }
+
+
+    }
+}
